066f624202f534e868666ce92483e9ad094d483a
RohanMaharjan123 <nahor.uzamaki@gmail.com>
2024-01-26 10:10:52 +0545

    creating chirps in laravel

--- project-app/routes/web.php
+++ project-app/routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use App\Http\Controllers\ChirpController;
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -28,4 +29,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+
+Route::resource('chirps', ChirpController::class)
+    ->only(['index', 'store'])
+    ->middleware(['auth', 'verified']);
 require __DIR__.'/auth.php';
